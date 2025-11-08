2689e1bfd7fefce8bf81c44b7de33efe3fd31aac
Danah92 <146811141+Danah92@users.noreply.github.com>
2025-11-08 19:29:10 +0300

    Fix SQL query to reference correct alias for totals

--- answer.sql
+++ answer.sql
@@ -1,7 +1,7 @@
-SELECT
+SELECT 
     inv.investor_id,
     sec.sector_name,
-    ROUND((inv.no_of_shares * 100.0) / total.total_shares, 2) AS share_percentage
+    ROUND((inv.no_of_shares * 100.0) / totals.total_shares, 2) AS share_percentage
 FROM investor_transactions inv
 JOIN sectors sec
     ON inv.sector_id = sec.sector_id
@@ -9,8 +9,6 @@ JOIN (
     SELECT investor_id, SUM(no_of_shares) AS total_shares
     FROM investor_transactions
     GROUP BY investor_id
-) total
-    ON inv.investor_id = total.investor_id
-ORDER BY
-    inv.investor_id,
-    share_percentage DESC;
+) AS totals
+    ON inv.investor_id = totals.investor_id
+ORDER BY inv.investor_id, share_percentage DESC;
