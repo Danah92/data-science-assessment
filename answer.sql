5d8d47fca3717e1bbdba5825a0b2cf97d98d5e70
Danah92 <146811141+Danah92@users.noreply.github.com>
2025-11-08 18:48:40 +0300

    Add SQL query to calculate share percentage by sector

--- answer.sql
+++ answer.sql
@@ -1,1 +1,10 @@
--- ADD YOUR SQL QUERY HERE
+SELECT 
+    inv.investor_id,
+    sec.sector_name,
+    ROUND(
+        (inv.no_of_shares / SUM(inv.no_of_shares) OVER (PARTITION BY inv.sector_id)) * 100,
+        2
+    ) AS share_percentage
+FROM Investments inv
+JOIN Sectors sec 
+ON inv.sector_id = sec.sector_id;
