6042f0b006524e69faa8a3c5ba438b469f20a2d6
Danah92 <146811141+Danah92@users.noreply.github.com>
2025-11-08 19:33:25 +0300

    Refactor SQL query to use window functions

--- answer.sql
+++ answer.sql
@@ -1,16 +1,8 @@
-SELECT
-    inv.investor_id,
-    sec.sector_name,
-    ROUND((inv.no_of_shares * 1.0 / totals.total_shares) * 100, 2) AS share_percentage
-FROM investor_transactions inv
-JOIN sectors sec
-    ON inv.sector_id = sec.sector_id
-JOIN (
-    SELECT investor_id, SUM(no_of_shares) AS total_shares
-    FROM investor_transactions
-    GROUP BY investor_id
-) AS totals
-    ON inv.investor_id = totals.investor_id
-ORDER BY 
-    inv.investor_id,
-    share_percentage DESC;
+SELECT 
+    it.investor_id,
+    s.sector_name,
+    ROUND( (it.no_of_shares * 100.0 / SUM(it.no_of_shares) OVER (PARTITION BY it.investor_id)), 2) AS Share_percentage
+FROM investor_transactions it
+JOIN sectors s
+    ON it.sector_id = s.sector_id
+ORDER BY it.investor_id, Share_percentage DESC;
