90a4046a664fe9db86a434857a98da22efc42061
Danah92 <146811141+Danah92@users.noreply.github.com>
2025-11-08 19:26:05 +0300

    Update share percentage calculation in SQL query
    Refactor share percentage calculation to use window function.

--- answer.sql
+++ answer.sql
@@ -2,16 +2,9 @@ SELECT
     inv.investor_id,
     sec.sector_name,
     ROUND(
-        (inv.no_of_shares * 100.0) / total.total_shares,
+        (inv.no_of_shares * 100.0) / SUM(inv.no_of_shares) OVER (PARTITION BY inv.investor_id),
         2
     ) AS share_percentage
 FROM investor_transactions inv
-JOIN sectors sec 
-    ON inv.sector_id = sec.sector_id
-JOIN (
-    -- total shares per investor
-    SELECT investor_id, SUM(no_of_shares) AS total_shares
-    FROM investor_transactions
-    GROUP BY investor_id
-) total
-    ON inv.investor_id = total.investor_id;
+JOIN sectors sec
+    ON inv.sector_id = sec.sector_id;
