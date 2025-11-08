2675e93af4e1563dba564667eed2905d96608059
Danah92 <146811141+Danah92@users.noreply.github.com>
2025-11-08 19:16:50 +0300

    Refactor SQL query to calculate share percentage

--- answer.sql
+++ answer.sql
@@ -1,10 +1,17 @@
-SELECT 
+SELECT
     inv.investor_id,
     sec.sector_name,
     ROUND(
-        (inv.no_of_shares / SUM(inv.no_of_shares) OVER (PARTITION BY inv.investor_id)) * 100,
+        (inv.no_of_shares * 100.0) / total.total_shares,
         2
     ) AS share_percentage
 FROM investor_transactions inv
 JOIN sectors sec 
-ON inv.sector_id = sec.sector_id;-- ADD YOUR SQL QUERY HERE
+    ON inv.sector_id = sec.sector_id
+JOIN (
+    -- total shares per investor
+    SELECT investor_id, SUM(no_of_shares) AS total_shares
+    FROM investor_transactions
+    GROUP BY investor_id
+) total
+    ON inv.investor_id = total.investor_id;
